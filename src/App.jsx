e566767bf1b2abd5bdd4bdb05dd19fbbbeabc6fa
Chizycodes <okwudirejoy@gmail.com>
2024-05-27 16:34:06 +0100

    Add seperator image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
-import {NavBar, Hero} from './components'
+import { NavBar, Hero } from './components';
+import UnionImg from './assets/union.svg';
 
 function App() {
 	return (
-		<div className="">
-			<NavBar />
-			<Hero />
-		</div>
+		<>
+			<div className="max-w-[1280px] mx-auto pt-8">
+				<NavBar />
+				<Hero />
+			</div>
+			<img src={UnionImg} alt="Seperator" className="mt-20 w-full" />
+		</>
 	);
 }
 
