83c0fceee93fcd46d29758584d46c32f8cd7ff90
Chizycodes <okwudirejoy@gmail.com>
2024-05-28 06:42:00 +0100

    fix Import issue

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Menu from './menu';
+import Menu from './NavMenu';
 import Logo from '../../assets/logo.svg';
 import { Link, NavLink } from 'react-router-dom';
 import { MenuIcon } from '../../assets/svgIcons';
