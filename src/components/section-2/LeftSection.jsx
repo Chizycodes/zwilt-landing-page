189e3566ac685d6a0c695733e5579b1f13f4af93
Chizycodes <okwudirejoy@gmail.com>
2024-05-29 05:01:47 +0100

    fix font style

--- src/components/section-2/LeftSection.jsx
+++ src/components/section-2/LeftSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LeftSection = ({item}) => {
   return (
 		<div className="">
-			<p className="text-2xl mb-4">{item.title}</p>
+			<p className="text-lg md:text-2xl font-medium mb-4">{item.title}</p>
 			<div className="flex lg:justify-between flex-wrap gap-5 text-[#202229CC]">
 				{item.items.map((s, i) => (
 					<span key={i} className="flex items-center gap-2">
